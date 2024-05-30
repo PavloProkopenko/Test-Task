2ccdebafcc21102bf4a4d0545331f51e9aa0e25a
Pavlo Prokopenko <stpasha6@gmail.com>
2024-05-30 15:24:39 +0300

    Added splash screen

--- App.js
+++ App.js
@@ -1,18 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useState, useEffect } from 'react';
+import Animated, {Easing, useSharedValue, useAnimatedStyle, withTiming, runOnJS} from 'react-native-reanimated';
 
+import SplashScreen from './SplashScreenView';
 import WelcomeScreen from "./screens/WelcomeScreen";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [isShowSplash, setIsShowSplash] = useState(true);
+  const opacity = useSharedValue(1);
+
+  useEffect(() => {
+    setTimeout(() => {
+      opacity.value = withTiming(0, {
+        duration: 1000,
+        easing: Easing.linear,
+      }, (isFinished) => {
+        if (isFinished) {
+          runOnJS(setIsShowSplash)(false);
+        }
+      });
+    }, 1000);
+  }, []);
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      opacity: opacity.value,
+    };
+  });
+
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Welcome" component={WelcomeScreen} options={{headerShown: false }}/>
-      </Stack.Navigator>
+      {isShowSplash ? (
+        <Animated.View style={[styles.container, animatedStyle]}>
+          <SplashScreen />
+        </Animated.View>
+      ) : (
+        <Stack.Navigator>
+          <Stack.Screen name="Welcome" component={WelcomeScreen} options={{ headerShown: false }} />
+        </Stack.Navigator>
+      )}
     </NavigationContainer>
   );
 }
@@ -20,8 +50,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,7 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import SplashScreen from '../SplashScreenView';
+
+export default function WelcomeScreen() {
+  
+
   return (
     <View style={styles.container}>
       <Text>It works!</Text>
